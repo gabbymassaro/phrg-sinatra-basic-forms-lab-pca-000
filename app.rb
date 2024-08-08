f12cf78943d6be9a8b8fde91576c380adc3d874f
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-08 10:21:25 -0400

    Pass all tests...again

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
+require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/new' do
+    erb :new
+  end
+
+  post '/display_puppy' do
+    erb :display_puppy
+  end
 end
